dc0daec7e0600dd2b3ebb616c084a6c5bb08c7f2
junkai yang <junkai.yang@griffithuni.edu.au>
2021-03-30 16:36:10 +0800

    climate create framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./dashboard/dashboard.module').then(mod => mod.DashboardModule)
+  },
+  {
+    path: 'graph',
+    loadChildren: () => import('./graph/graph.module').then(mod => mod.GraphModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
