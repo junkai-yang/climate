2a258dcbee1de369c6d2b37211bf041b4c81a035
junkai-yang <67625632+junkai-yang@users.noreply.github.com>
2021-04-23 16:54:46 +0800

    change date format

--- src/app/layout/breadcrumb/breadcrumb.component.ts
+++ src/app/layout/breadcrumb/breadcrumb.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {Router, ActivatedRoute, NavigationEnd} from "@angular/router";
-import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
+import {NzDatePickerComponent} from 'ng-zorro-antd/date-picker';
 
 @Component({
   selector: 'app-breadcrumb',
@@ -13,6 +13,15 @@ export class BreadcrumbComponent implements OnInit {
   dateFormat = 'yyyy/MM/dd';
   date;
 
+  start;
+  end;
+  startYear;
+  startMonth;
+  startDay;
+  endYear;
+  endMonth;
+  endDay;
+  returnDate;
 
 
   constructor(
@@ -27,14 +36,14 @@ export class BreadcrumbComponent implements OnInit {
         this.routerPath = data.url.substr(1) === "" ? "home/content" : data.url.substr(1);
         this.routerSplit = this.routerPath.split('/')
         console.log("Router方式:", this.routerPath);
-        let graphs:any = JSON.parse(localStorage.getItem('ContainGraph')) || {'Graph':[]}
+        let graphs: any = JSON.parse(localStorage.getItem('ContainGraph')) || {'Graph': []}
         // console.log(graphs)
         if (this.routerSplit[0] === 'home') {
           // console.log(graphs)
-          if (graphs.Graph === [] || graphs.Graph.indexOf(this.routerSplit[1]) == -1){
+          if (graphs.Graph === [] || graphs.Graph.indexOf(this.routerSplit[1]) == -1) {
             graphs.Graph.push(this.routerSplit[1])
             console.log(graphs)
-            localStorage.setItem('ContainGraph',JSON.stringify(graphs))
+            localStorage.setItem('ContainGraph', JSON.stringify(graphs))
           }
         }
 
@@ -47,16 +56,55 @@ export class BreadcrumbComponent implements OnInit {
   }
 
 
-
   emitDate() {
-    if (this.date){
-      const date = {
-        'start': this.date[0],
-        'end': this.date[1]
+    if (this.date) {
+      this.start = this.date[0]
+      this.end = this.date[1]
+      this.startYear = this.start.getFullYear()
+      this.startMonth = this.start.getMonth() + 1
+      this.startDay = this.start.getDate()
+      this.endYear = this.end.getFullYear()
+      this.endMonth = this.end.getMonth() + 1
+      this.endDay = this.end.getDate()
+      // console.log(typeof this.endDay)
+      if (this.endYear - this.startYear > 0) {    // more than 1 year
+        for (let i = 0; i < this.endYear - this.startYear + 1; i++) {
+          if (this.startYear + i === this.startYear) {  // is start year
+            this.returnDate = {
+              [this.startYear]: {
+                s_d: this.startDay,
+                s_m: this.startMonth,
+                e_d: 31,
+                e_m: 12
+              }
+            }
+          } else if (this.startYear + i === this.endYear) { // is end year
+            this.returnDate[this.endYear] = {
+              s_d: 1,
+              s_m: 1,
+              e_d: this.endDay,
+              e_m: this.endMonth
+            }
+          } else {    // is mid year
+            this.returnDate[this.startYear + i] = {
+              s_d: 1,
+              s_m: 1,
+              e_d: 31,
+              e_m: 12
+            }
+          }
+        }
+      } else {  // same year
+        this.returnDate = {
+          [this.startYear]: {
+            s_d: this.startDay,
+            s_m: this.startMonth,
+            e_d: this.endDay,
+            e_m: this.endMonth
+          }
+        }
       }
-      console.log(date)
+    console.log(this.returnDate)
     }
-
   }
-
 }
